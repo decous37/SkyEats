f10935c713d965a6ebe2daa2c4f95c0bf7e367bd
Decous <244276117+decous1622-stack@users.noreply.github.com>
2026-05-12 21:33:24 +0800

    苍穹外卖项目初始代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -12,12 +12,52 @@ import com.sky.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+import com.sky.dto.EmployeeDTO;
+import com.sky.constant.PasswordConstant;
+import com.sky.context.BaseContext;
+import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDateTime;
+
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeMapper employeeMapper;
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     *
+     *
+     */
+    public void save(EmployeeDTO employeeDTO) {
+        //1.创建Employee实体对象
+        Employee employee = new Employee();
+
+
+        //2.复制EmployeeDTO中的属性
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        //3.设置默认密码
+        employee.setPassword(PasswordConstant.DEFAULT_PASSWORD);
+
+        //4.设置默认状态
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //5.设置创建时间和更新时间
+        LocalDateTime now = LocalDateTime.now();
+        employee.setCreateTime(now);
+        employee.setUpdateTime(now);
+
+        //6.设置创建人和修改人
+        long currentId = BaseContext.getCurrentId();
+        employee.setCreateUser(currentId);
+        employee.setUpdateUser(currentId);
+        //7.调用Mapper插入数据库
+    }
 
     /**
      * 员工登录
@@ -38,8 +78,8 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
-        //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        //密码比对：前端传来的是明文密码，数据库中保存的是 MD5 摘要
+//        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
